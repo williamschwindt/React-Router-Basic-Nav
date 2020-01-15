cbaaf140ec6f95b54e81439be9db63be3f136113
William Schwindt <william@Williams-MacBook-Pro.local>
2020-01-15 09:21:40 -0800

    routes declaired

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+    <Route exact path="/home" component={Home} />
+    <Route exact path="/about" component ={About} />
+    <Route exact path="/contact" component ={Contact} />
+    </div>
+  );
+};
 
 export default App;
